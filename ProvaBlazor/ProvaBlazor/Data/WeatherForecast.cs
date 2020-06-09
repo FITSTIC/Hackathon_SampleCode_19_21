ef2606df236a802e6cc70aecf4d8d3f624e69e10
matteo.venturi7 <matteo.venturi7@gmail.com>
2020-06-09 15:09:13 +0200

    modifica e inserimento previsioni

--- ProvaBlazor/ProvaBlazor/Data/WeatherForecast.cs
+++ ProvaBlazor/ProvaBlazor/Data/WeatherForecast.cs
@@ -9,12 +9,16 @@ namespace ProvaBlazor.Data
         public int Id { get; set; }
         public DateTime Date { get; set; }
 
+        [Range(-40, 65)]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        [Required]
         public string Summary { get; set; }
 
+        [Required]
+        public int CityId { get; set; }
         public City City { get; set; }
 
         public WeatherForecast()
